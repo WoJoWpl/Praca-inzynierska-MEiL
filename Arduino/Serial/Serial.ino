a318b5e334f314a98753a51b890d89f8ae908926
Wojciech Witoń <witon.wojciech@gmail.com>
2016-02-05 00:11:57 +0100

    Wersje na dzień oddania pracy

--- Arduino/Serial/Serial.ino
+++ Arduino/Serial/Serial.ino
@@ -1,4 +1,9 @@
+//Przesylana wiadomosc
 String tekst;
+//Okresla, do ktorego robota ma trafic wiadomosc
+//robot = 1 - do robota nr 1
+//robot = 2 - do robota nr 2
+int robot = 0;
 
 void setup() {
   //USB
@@ -14,19 +19,21 @@ void setup() {
 void loop() {
   
   //Jezeli przyszla wiadomosc ze sterownika Keyence,
-  //przeslij ja do obydwu robotow
+  //przeslij ja do robota, ktory nawiazal polaczenie
   if(Serial1.available())
   {
     tekst = Serial1.readStringUntil('\r');
     Serial.println("Keyence: " + tekst);
-    Serial2.println(tekst);
-    Serial3.println(tekst);
+    if(robot == 1) Serial2.println(tekst);
+    else if(robot == 2) Serial3.println(tekst);
+    robot = 0;
   }
 
   //Jezeli przyszla wiadomosc od robota nr 1,
   //przeslij ja do sterownika Keyence
   if(Serial2.available())
   {
+    robot = 1;
     tekst = Serial2.readStringUntil('\r');
     Serial.println("Robot nr 1: " + tekst);
     Serial1.println(tekst);
@@ -36,6 +43,7 @@ void loop() {
   //przeslij ja do sterownika Keyence
   if(Serial3.available())
   {
+    robot = 2;
     tekst = Serial3.readStringUntil('\r');
     Serial.println("Robot nr 2: " + tekst);
     Serial1.println(tekst);
